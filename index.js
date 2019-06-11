2682f745024a152cf8a7545d6e4ed9f4f2d79867
Sachin Mahajan <sachinm78@gmail.com>
2019-06-11 22:20:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,6 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  // Alternate using ES6 Spread operators:
-  // return { ...driver, ...{ [key]: value } }
   return Object.assign({}, driver, { [key]: value });
 }
 
